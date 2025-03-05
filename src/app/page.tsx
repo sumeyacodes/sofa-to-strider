e07bbf4ef34970679be342f8531c5a7ea1b86cc4
Alistair Lambert <lambert.alistair96@gmail.com>
2025-03-05 10:37:38 +0000

    Carousel compoent added for the maps to populate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Carousel } from "./components/ui/carousel"; // Adjust the import path as needed
 
 export default function Home() {
+  const [slides, setSlides] = useState([
+    {
+      title: "Slide 1",
+
+      // src: URL HERE,
+    },
+    {
+      title: "Slide 2",
+
+      // src: URL HERE,
+    },
+    {
+      title: "Slide 3",
+
+      // src: URL HERE,
+    },
+    {
+      title: "Slide 4",
+
+      // src: URL HERE,
+    },
+  ]);
+
   return (
     <main className="flex flex-col items-center justify-center h-screen gap-10">
       <Image
@@ -11,6 +38,7 @@ export default function Home() {
         priority
       />
       <h1 className="text-5xl font-bold text-center">Sofa to Strider</h1>
+      {slides.length > 0 && <Carousel slides={slides} />}
     </main>
   );
 }
